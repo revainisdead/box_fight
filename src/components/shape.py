d911ee22ca16378b6c2057795a74f83c87469244
Christian Hall <hallbasics@yahoo.com>
2017-07-26 16:40:18 -0700

    Tint the random colors.

--- src/components/shape.py
+++ src/components/shape.py
@@ -11,14 +11,27 @@ class Cube:
     def __init__(self, pos=(0, 0, 0)):
         x, y, z = pos
         self.verts = [(x+X/2, y+Y/2, z+Z/2) for X, Y, Z in self.verts]
-        self.colors = self.randomize_color()
+
+        # Each tint application lightens slightly
+        tint_factor = 0.1
+        self.colors = self.randomize_color(tint_factor)
 
 
-    def randomize_color(self) -> Tuple[Tuple[int, int, int], ...]:
-        for _ in range(3):
-            c = ((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),)
-        return c * 6
+    def randomize_color(self, tint_factor: float) -> Tuple[Tuple[int, int, int], ...]:
+        """ Get a random color and then tint that color for each side. """
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
 
+        colors = []
+        for _ in range(6):
+            # Change tint for each side
+            r = r + (255 - r) * tint_factor
+            g = g + (255 - g) * tint_factor
+            b = b + (255 - b) * tint_factor
+            colors.append((r, g, b))
+
+        return tuple(colors)
 
 
 
